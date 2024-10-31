429bacd37d0dda977e8ff19df682f6ffd77770ab
gcapaross <gvds@ua.pt>
2024-10-31 09:39:18 +0000

    Endpoints

--- Project/RestAPI/src/main/java/ies/carbox/api/RestAPI/controller/CarController.java
+++ Project/RestAPI/src/main/java/ies/carbox/api/RestAPI/controller/CarController.java
@@ -1,4 +1,4 @@
-package ies.carbox.api.RestAPI.entity;
+package ies.carbox.api.RestAPI.controller;
 
 import ies.carbox.api.RestAPI.entity.Car;
 import ies.carbox.api.RestAPI.service.CarService;
@@ -20,17 +20,43 @@ public class CarController {
     }
 
     // 1. Create a new Car
+    @PostMapping
+    public ResponseEntity<Car> createCar(@Valid @RequestBody Car car) {
+        carService.createCar(car);
+        return ResponseEntity.ok(car);
+    }
 
     
     // 2. Get a list of all Cars
+    @GetMapping
+    public ResponseEntity<List<Car>> getAllCars() {
+        List<Car> cars = carService.getAllUserCars();
+        return ResponseEntity.ok(cars);
+    }
+
 
 
     // 3. Get a single Car by its ID
+    @GetMapping("/{carId}")
+    public ResponseEntity<Car> getCarById(@PathVariable String carId) {
+        Car car = carService.getCarUserById(carId);
+        if (car != null) {
+            return ResponseEntity.ok(car);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 
     // 4. Update an existing Car
+    @PutMapping("/{carId}")
+    // TODO:
 
 
     // 5. Delete a Car by its ID
+    @DeleteMapping("/{carId}")
+    public void deleteCar(@PathVariable String carId) {
+        carService.deleteCarById(carId);
+    }
 
 }
