4a8e51b1565a4a70e77b4979dd904af134e99b63
Affapple <joaopmgaspar@ua.pt>
2024-11-18 19:20:10 +0000

    fixes

---
+++ Project/Frontend/Carbox/carbox/src/components/Footer.tsx
@@ -0,0 +1,19 @@
+function Footer() {
+    return (
+        <footer className="bg-gray-200 py-6 text-center text-sm text-gray-500 mt-10 border-t">
+            <div className="flex justify-center space-x-4">
+                <a href="#" className="hover:underline">
+                    Help Center
+                </a>
+                <a href="#" className="hover:underline">
+                    Terms of Service
+                </a>
+                <a href="#" className="hover:underline">
+                    Privacy Policy
+                </a>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ Project/Frontend/Carbox/carbox/src/pages/AdminPage.tsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+import Navbar from "components/NavBar";
+import Footer from "components/Footer";
+
+function AdminPage() {
+  return (
+    <>
+      <Navbar />
+
+      <Footer />
+    </>
+  );
+}
+
+export default AdminPage;
