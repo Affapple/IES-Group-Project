10a7b270613e6b7adb2251449783eb081d7a78d5
BirrasOBacano <birras13@sapo.pt>
2024-11-29 17:34:18 +0000

    Updated code according to guidelines (camelCase)

--- Project/Database/DatabaseAdapter.py
+++ Project/Database/DatabaseAdapter.py
@@ -9,10 +9,10 @@ def connect(url, username, password):
     logging.info("Connected to database!")
     return mongo_client
 
-def get_db(client, db_name):
+def getDb(client, db_name):
     return client[db_name]
 
-def check_collections(db):
+def checkCollections(db):
     for col in db.list_collection_names():
         logging.info(f"Collection: {col}")
 
@@ -25,91 +25,91 @@ def check_collections(db):
     if "TripInfos" not in db.list_collection_names():
         db.create_collection("TripInfos")
 
-def check_car_exists(db, car):
+def checkCarExists(db, car):
     if db["Cars"].find_one({"ecu_id":car["ecu_id"]})==None:
         db["Cars"].insert_one(car)
         logging.info("Car inserted successfully!")
     else:
         logging.info("Car already exists!")
 
-def check_for_errors(live_data):
-    if live_data["errors"]!=[]:
-        logging.error(f"Error: {live_data['errors']}")
+def checkForErrors(liveData):
+    if liveData["errors"]!=[]:
+        logging.error(f"Error: {liveData['errors']}")
         return True
     
-def insert_car_data(db, data):
+def insertCarData(db, data):
     db["Cars"].insert_one(data)
     logging.info("Car data inserted successfully!")
 
-def insert_car_live_data(db, data):
+def insertCarLiveData(db, data):
     db["CarLiveInfo"].insert_one(data)
     logging.info("Car live data inserted successfully!")
 
-def insert_trip_data(db, data):
+def insertTripData(db, data):
     db["TripInfos"].insert_one(data)
     logging.info("Trip data inserted successfully!")
 
 #Checks last live data of car and sees if its a new trip or the end of one
-def check_trip(db, live_data):
+def checkTrip(db, liveData):
     #Get last trip of car and sort by trip number
-    last_trip = db["TripInfos"].find({"car_id":live_data["car_id"]}).sort("trip_id", -1).limit(1)
-    if last_trip.count()==0:
-        if live_data["car_status"]:
+    lastTrip = db["TripInfos"].find({"car_id":liveData["car_id"]}).sort("trip_id", -1).limit(1)
+    if lastTrip.count()==0:
+        if liveData["car_status"]:
             #Its the first trip ever
-            live_data["trip_id"]=1
-            create_trip(db, live_data, 1)
-            return live_data
+            liveData["trip_id"]=1
+            createTrip(db, liveData, 1)
+            return liveData
     else:
-        last_trip = last_trip[0]
+        lastTrip = lastTrip[0]
         #If trip has not ended yet
-        if last_trip["trip_end"]==None:
-            if live_data["car_status"]:
-                live_data["trip_id"]=last_trip["trip_id"]
-                return live_data
+        if lastTrip["trip_end"]==None:
+            if liveData["car_status"]:
+                liveData["trip_id"]=lastTrip["trip_id"]
+                return liveData
             else:
-                last_trip["trip_end"]=live_data["timestamp"]
-                live_data["trip_id"]=last_trip["trip_id"]
+                lastTrip["trip_end"]=liveData["timestamp"]
+                liveData["trip_id"]=lastTrip["trip_id"]
                 #Get all the speeds, rpm, motortemp and torque of trip
-                trip_data = db["CarLiveInfo"].find({"car_id":live_data["car_id"], "trip_id":last_trip["trip_id"]}).sort("timestamp", 1)
+                trip_data = db["CarLiveInfo"].find({"car_id":liveData["car_id"], "trip_id":lastTrip["trip_id"]}).sort("timestamp", 1)
                 speeds = []
                 rpm = []
-                motortemp = []
+                motorTemp = []
                 torque = []
                 for data in trip_data:
                     speeds.append(data["speed"])
                     rpm.append(data["rpm"])
-                    motortemp.append(data["motor_temperature"])
+                    motorTemp.append(data["motor_temperature"])
                     torque.append(data["torque"])
-                last_trip["trip_speeds"]=speeds
-                last_trip["trip_rpm"]=rpm
-                last_trip["trip_motor_temp"]=motortemp
-                last_trip["trip_torque"]=torque
-                db["TripInfos"].update_one({"car_id":live_data["car_id"], "trip_id":last_trip["trip_id"]}, {"$set":last_trip})
-                return live_data
+                lastTrip["trip_speeds"]=speeds
+                lastTrip["trip_rpm"]=rpm
+                lastTrip["trip_motor_temp"]=motorTemp
+                lastTrip["trip_torque"]=torque
+                db["TripInfos"].update_one({"car_id":liveData["car_id"], "trip_id":lastTrip["trip_id"]}, {"$set":lastTrip})
+                return liveData
         else:
-            live_data["trip_id"]=last_trip["trip_id"]+1
-            create_trip(db, live_data, last_trip["trip_id"]+1)
-            return live_data
+            liveData["trip_id"]=lastTrip["trip_id"]+1
+            createTrip(db, liveData, lastTrip["trip_id"]+1)
+            return liveData
 
-def create_trip(db, live_data, num):
-    trip={"car_id":live_data["car_id"], "trip_id":num, "trip_start":live_data["timestamp"], "trip_end":None}
-    insert_trip_data(db, trip)
+def createTrip(db, liveData, num):
+    trip={"car_id":liveData["car_id"], "trip_id":num, "trip_start":liveData["timestamp"], "trip_end":None}
+    insertTripData(db, trip)
     return trip
     
     
 #TODO: Implement this function to send alerts to the notification handler
-def notify_user(live_data):
+def notifyUser(liveData):
     pass
 
 #TODO Implement this function to read messages from the queue
 def readQueue():
     return "Not reading messages yet, but program working"
 
-def load_cars():
+def loadCars():
     #Given the data is simulated, the cars must be preloaded in the database
     #If we generated new cars from 0, we would need a way for people on the frontend to know their new car's ecu_id
-    Cars=[]
-    Cars.append({
+    cars=[]
+    cars.append({
         'ecu_id': '12345ABCDEF',
         'brand': 'Toyota',
         'model': 'Corolla',
@@ -123,7 +123,7 @@ def load_cars():
         'horsepower': 120,
         'autonomy': 600,
     })
-    Cars.append({
+    cars.append({
         'ecu_id': '67890FGHIJK',
         'brand': 'Honda',
         'model': 'Civic',
@@ -137,7 +137,7 @@ def load_cars():
         'horsepower': 158,
         'autonomy': 550,
     })
-    Cars.append({
+    cars.append({
         'ecu_id': '24680LMNOPQ',
         'brand': 'Ford',
         'model': 'Focus',
@@ -151,7 +151,7 @@ def load_cars():
         'horsepower': 150,
         'autonomy': 700,
     })
-    Cars.append({
+    cars.append({
         'ecu_id': '13579QRSTUV',
         'brand': 'Chevrolet',
         'model': 'Malibu',
@@ -165,7 +165,7 @@ def load_cars():
         'horsepower': 197,
         'autonomy': 580,
     })
-    Cars.append({
+    cars.append({
         'ecu_id': '98765WXYZAB',
         'brand': 'BMW',
         'model': '3 Series',
@@ -179,7 +179,7 @@ def load_cars():
         'horsepower': 255,
         'autonomy': 520,
     })
-    Cars.append({
+    cars.append({
         'ecu_id': '54321CDEFGH',
         'brand': 'Mercedes',
         'model': 'C-Class',
@@ -193,7 +193,7 @@ def load_cars():
         'horsepower': 194,
         'autonomy': 800,
     })
-    Cars.append({
+    cars.append({
         'ecu_id': '11223IJKLMN',
         'brand': 'Audi',
         'model': 'A4',
@@ -207,7 +207,7 @@ def load_cars():
         'horsepower': 201,
         'autonomy': 550,
     })
-    Cars.append({
+    cars.append({
         'ecu_id': '99887OPQRST',
         'brand': 'Tesla',
         'model': 'Model 3',
@@ -221,7 +221,7 @@ def load_cars():
         'horsepower': 283,
         'autonomy': 560,
     })
-    Cars.append({
+    cars.append({
         'ecu_id': '55667UVWXYZ',
         'brand': 'Volkswagen',
         'model': 'Passat',
@@ -235,29 +235,29 @@ def load_cars():
         'horsepower': 174,
         'autonomy': 750,
     })
-    return Cars
+    return cars
 
 #Main program
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     logging.info("Starting Database Adapter...")
     db=connect("mongodb://db:27017/", "carbox", "mySecretPassword")
-    db=get_db(db, "carbox")
-    check_collections(db)
-    cars=load_cars()
+    db=getDb(db, "carbox")
+    checkCollections(db)
+    cars=loadCars()
     for car in cars:
-        check_car_exists(db, car)
+        checkCarExists(db, car)
 
     #TODO Wait for messages and update the database
     while True:
         continue
-        live_data=readQueue()
-        logging.info(f"Data received: {live_data}")
-        if check_for_errors(live_data):
-            notify_user(live_data)
+        liveData=readQueue()
+        logging.info(f"Data received: {liveData}")
+        if checkForErrors(liveData):
+            notifyUser(liveData)
         else:
-            live_data=check_trip(db, live_data)
-            insert_car_live_data(db, live_data)
+            liveData=checkTrip(db, liveData)
+            insertCarLiveData(db, liveData)
 
 
     
